e7e1ac7c372793bc3e726a9e13e6a647cf7bfaad
Makhal.W <baruur.co@gmail.com>
2022-04-12 22:52:33 -0500

    Initial commit

---
+++ src/main/java/com/perscholas/tutorstudentmanagmentsystems/TutorStudentManagmentSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.tutorstudentmanagmentsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorStudentManagmentSystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorStudentManagmentSystemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/perscholas/tutorstudentmanagmentsystems/TutorStudentManagmentSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.tutorstudentmanagmentsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorStudentManagmentSystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
